b2757ac4f11d69ea97a623b63785a16a347c7ae9
Stephen <stephen@umw.edu>
2024-12-09 19:07:19 -0500

    Finish comments interface.

--- routes.py
+++ routes.py
@@ -1,6 +1,20 @@
 
 from flask import render_template, request, redirect, url_for
 from comms import comments
+from redis import Redis
+
 @comments.route("/")
 def landing():
-    return "Hello world!"
+    r = Redis(decode_responses=True)
+    students = r.smembers("students")
+    return render_template("comments.html", students=sorted(list(students)))
+
+
+@comments.route("/add_comment", methods=["POST"])
+def add_comment():
+    comment = request.form['comment']
+    student = request.form['student']
+    r = Redis(decode_responses=True)
+    r.rpush(f"feedback:{student}", comment)
+    return redirect(url_for('landing'))
+
